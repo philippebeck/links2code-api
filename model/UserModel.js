8c80a8363de32776c99e1f7ec1f6dfc25c839a4a
Philippe Beck <philippe@philippebeck.net>
2022-05-04 15:59:15 +0200

    Refacto models

--- model/UserModel.js
+++ model/UserModel.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const mongoose        = require("mongoose");
+const mongoose = require("mongoose");
 const uniqueValidator = require("mongoose-unique-validator");
 
 const UserModel = mongoose.Schema(
