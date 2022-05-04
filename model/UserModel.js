471bcd3009e6e0d5f5f2a95e9ce05f24f63d6fc1
Philippe Beck <philippe@philippebeck.net>
2022-05-04 13:44:37 +0200

    Create UserModel

---
+++ model/UserModel.js
@@ -0,0 +1,30 @@
+"use strict";
+
+const mongoose        = require("mongoose");
+const uniqueValidator = require("mongoose-unique-validator");
+
+const UserModel = mongoose.Schema(
+  {
+    name: { 
+      type: String, 
+      required: true,
+      unique: true
+    },
+    email: { 
+      type: String, 
+      required: true,
+      unique: true
+    },
+    pass: { 
+      type: String, 
+      required: true 
+    }
+  },
+  {
+    versionKey: false
+  }
+);
+
+UserModel.plugin(uniqueValidator);
+
+module.exports = mongoose.model("User", UserModel);
