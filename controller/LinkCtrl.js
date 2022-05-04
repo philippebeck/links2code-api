e1f533eb31b431c8b83efa958301b0fb81497558
Philippe Beck <philippe@philippebeck.net>
2022-05-04 15:59:33 +0200

    Refacto controllers

--- controller/LinkCtrl.js
+++ controller/LinkCtrl.js
@@ -2,6 +2,8 @@
 
 const LinkModel = require("../model/LinkModel");
 
+require("dotenv").config();
+
 /**
  * LIST LINK
  * @param {*} req 
@@ -10,9 +12,11 @@ const LinkModel = require("../model/LinkModel");
 exports.list = (req, res) => {
   LinkModel
     .find()
-    .then(links => res.status(200).json(links))
-    .catch(error => res.status(400).json({ error }));
+    .then((links) => res.status(200).json(links))
+    .catch((error) => res.status(400).json({ error }));
 };
+
+// ******************** CRUD ******************** //
 
 /**
  * CREATE LINK
@@ -24,10 +28,8 @@ exports.create = (req, res) => {
 
   link
     .save()
-    .then(() => res.status(201).json(
-      { message: "Lien créé !" }
-    ))
-    .catch(error => res.status(400).json({ error }));
+    .then(() => res.status(201).json({ message: process.env.LINK_CREATED }))
+    .catch((error) => res.status(400).json({ error }));
 };
 
 /**
@@ -39,14 +41,9 @@ exports.update = (req, res) => {
   let link = req.body;
 
   LinkModel
-    .updateOne(
-      { _id: req.params.id }, 
-      { ...link, _id: req.params.id }
-    )
-    .then(() => res.status(200).json(
-      { message: "Lien modifié !" }
-    ))
-    .catch(error => res.status(400).json({ error }));
+    .updateOne({ _id: req.params.id }, { ...link, _id: req.params.id })
+    .then(() => res.status(200).json({ message: rocess.env.LINK_UPDATED }))
+    .catch((error) => res.status(400).json({ error }));
 };
 
 /**
@@ -57,8 +54,6 @@ exports.update = (req, res) => {
 exports.delete = (req, res) => {
   LinkModel
     .deleteOne({ _id: req.params.id })
-    .then(() => res.status(200).json(
-      { message: "Lien supprimé !" }
-    ))
-    .catch(error => res.status(400).json({ error }))
+    .then(() => res.status(200).json({ message: rocess.env.LINK_DELETED }))
+    .catch((error) => res.status(400).json({ error }))
 };
