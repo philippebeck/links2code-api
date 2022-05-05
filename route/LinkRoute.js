e434ac8cf81f1c320bad20857c6adb5ef6845c42
Philippe Beck <philippe@philippebeck.net>
2022-05-05 10:35:44 +0200

    Update routes for nemjs

--- route/LinkRoute.js
+++ route/LinkRoute.js
@@ -2,12 +2,12 @@
 
 const express = require("express");
 const router = express.Router();
-const auth = require("../middleware/auth");
+const nem = require("nemjs");
 const LinkCtrl = require("../controller/LinkCtrl");
 
 router.get("/", LinkCtrl.list);
-router.post("/", auth, LinkCtrl.create);
-router.put("/:id", auth, LinkCtrl.update);
-router.delete("/:id", auth, LinkCtrl.delete);
+router.post("/", nem.auth, LinkCtrl.create);
+router.put("/:id", nem.auth, LinkCtrl.update);
+router.delete("/:id", nem.auth, LinkCtrl.delete);
 
 module.exports = router;
