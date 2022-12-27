c1bcaf3f245a5dd33f5124b160f74e100eea8407
Philippe Beck <philippe@philippebeck.net>
2022-12-28 00:12:15 +0400

    Rename auth() to checkAuth()

--- route/UserRoute.js
+++ route/UserRoute.js
@@ -1,15 +1,15 @@
 "use strict";
 
-const express = require("express");
-const router = express.Router();
-const nem = require("nemjs");
-const UserCtrl = require("../controller/UserCtrl");
+const express   = require("express");
+const router    = express.Router();
+const nem       = require("nemjs");
+const UserCtrl  = require("../controller/UserCtrl");
 
-router.get("/", nem.auth, UserCtrl.list);
-router.post("/", nem.auth, UserCtrl.create);
+router.get("/", nem.checkAuth, UserCtrl.list);
+router.post("/", nem.checkAuth, UserCtrl.create);
 router.post("/login", UserCtrl.login);
-router.put("/:id", nem.auth, UserCtrl.update);
-router.delete("/:id", nem.auth, UserCtrl.delete);
+router.put("/:id", nem.checkAuth, UserCtrl.update);
+router.delete("/:id", nem.checkAuth, UserCtrl.delete);
 router.post("/send", UserCtrl.send);
 
 module.exports = router;
